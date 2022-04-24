3282912e7359bb5556a56a6c098a113b93d8e40c
Tyler Nodell <tyler.l.nodell@gmail.com>
2022-04-24 16:50:05 -0400

    Initial fflogs data pull

--- apollo-client.js
+++ apollo-client.js
@@ -1,6 +1,31 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
+var options = {
+    method: 'POST',
+    url: 'https://www.fflogs.com/oauth/token',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    data: {
+        grant_type: 'client_credentials',
+        client_id: process.env.FFLOGS_CLIENT_ID,
+        client_secret: process.env.FFLOGS_CLIENT_SECRET,
+    }
+};
+
+const authToken = async () => {
+    return await axios(options)
+        .then(function (response) {
+            // write token to process.env
+            process.env.FFLOGS_TOKEN = response.data.access_token;
+            return response.data.access_token;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+};
+
 
 const httpLink = createHttpLink({
     uri: 'https://www.fflogs.com/api/v2/client',
@@ -8,8 +33,7 @@ const httpLink = createHttpLink({
 
 
 const authLink = setContext((_, { headers }) => {
-    // get the authentication token from local storage if it exists else get it from getAccessToken
-    const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI5NWNlZTViMC01MWViLTQ2ODgtYmRjZi1lN2NjMmI4YzgwOWQiLCJqdGkiOiJhOWU5YzE1YWE1NTZmYTk4NGMzMTkxMzhmNDNiOTZlNzhlODkyMmQwZmE1ZDZmMGFiZGU4NzZkZmZlNzJkMDc5ZTFlNzRlMjQ5ODE1MmFiZiIsImlhdCI6MTY0ODk0NDQ5NC41MDYyOTcsIm5iZiI6MTY0ODk0NDQ5NC41MDYzLCJleHAiOjE2NTkzMTI0OTQuNDk4OTQxLCJzdWIiOiIiLCJzY29wZXMiOlsidmlldy11c2VyLXByb2ZpbGUiLCJ2aWV3LXByaXZhdGUtcmVwb3J0cyJdfQ.ptnHLAOHPHePRfJuAcOaW6bybDg7dowYjlrPqi21rB5iOB_DshpYdNwSDqiKl-TXCmEoJsCD_PujqQdEiv3JxY2RJcq4iq3o8HLThpIqc1GrYR7j7Jc94OnpRIZCIBj_5gmPlBZl-nfP2HlcCaLYfuVicP3Q6OMWwLLkYa5FQmy1fFlBL-ldfdpYkc3eMzIjfVEPYjDVsj8VbE1WjfSnNNsFFEAx_k9Vo36IH32MxbnvVv8_HOXXI7ZYNfoeYgzw98r16AgRbrQFSILjsG_bIgwKACn-yekMEpnYRXi2NmQVx2LkaT_e4BZWt7yBh4Ag3ZomPGiaM9rJudAFKcv87g-Gp1sSCvWiDn1Z5Re9uD8GLvZaLZWpKcP4PNwhDWItyGZs1dPWd4ozYvaLb04IuTovDLDVrSPdEQ-oxcmzJ7pewVkv8lUhSNRm161tTkOb75TAJ6vmqbhaXKcP-_vUkzSWr70zECgNZ94wBfEaaFUgGITC0Xxc05NhK5GwS42fJPCXhiT60PazhvU0tMbs53vIXPIpz8F_dp2T_HineBgd8VmbGOKxao0NZEZ7NRFEUuUrnGwsvgomLMhrsPvuVG30FflK5nyruXnSqU8G2mAzAZXDVojCepfLdegFCnF-P2pvniSNQDNH--14VsmKn-6cvxkIvSWxdPG-6xY3pgU";
+    const token = process.env.TOKEN
     return {
         headers: {
             ...headers,
--- pages/log.tsx
+++ pages/log.tsx
@@ -1,64 +1,106 @@
 import { gql } from "@apollo/client";
 import client from "../apollo-client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, ReactChild, ReactFragment, ReactPortal} from "react";
 
 export async function getStaticProps() {
-  const { data } = await client.query({
-    query: gql`
-        query ($code: String!, $startTime: Float!, $endTime: Float!) {
-            reportData {
-                report(code: $code){
-                    code
-                    fights(killType: Kills) {
-                        id
-                        encounterID
-                        startTime
-                        endTime
-                        kill
-                        maps {
+    const {data} = await client.query({
+        query: gql`
+            query ($code: String!, $startTime: Float!, $endTime: Float!) {
+                reportData {
+                    report(code: $code){
+                        code
+                        fights(killType: Kills) {
                             id
+                            encounterID
+                            startTime
+                            endTime
+                            kill
+                            maps {
+                                id
+                            }
                         }
-                    }
-                    events(
-                        startTime: $startTime,
-                        endTime: $endTime,
-                        dataType: Casts,
-                        useAbilityIDs: false,
-                        useActorIDs: false,
-                        sourceID: 6
-                    ) {
-                        data
+                        events(
+                            startTime: $startTime,
+                            endTime: $endTime,
+                            dataType: Casts,
+                            useAbilityIDs: false,
+                            useActorIDs: false,
+                            sourceID: 6
+                        ) {
+                            data
+                        }
                     }
                 }
             }
-        }
-    `,
-    variables: {
-      code: "nqcTBMzW1aDPtyGp",
-      startTime: 7413182,
-      endTime: 7823821,
-    },
-  });
+        `,
+        variables: {
+            code: "nqcTBMzW1aDPtyGp",
+            startTime: 7413182,
+            endTime: 7823821,
+        },
+    });
 
-  let fights = data.reportData.report.fights;
-  let code = data.reportData.report.code;
-  let casts = data.reportData.report.events.data;
+    let fights = data.reportData.report.fights;
+    let casts = data.reportData.report.events.data;
 
-  return {
-    props: {
-      fights,
-      casts,
-    },
-  };
+    return {
+        props: {
+            fights,
+            casts,
+        },
+    };
 }
 
-export default function Log( { fights, casts } ) {
+export default function Log({fights, casts}) {
+    const fight = fights[0];
+    const abilities = casts;
 
-    const [encounter, setEncounter] = useState(fights[0]);
-    const [abilities, setAbilities] = useState(casts);
+    const fightInstance = () => {
+        switch (fight.encounterID) {
+            case 78:
+                return "Asphodelos: The First Circle (Savage)";
+            case 79:
+                return "Asphodelos: The Second Circle (Savage)";
+            case 80:
+                return "Asphodelos: The Third Circle (Savage)";
+            case 81:
+                return "Asphodelos: The Fourth Circle (Savage)";
+            case 82:
+                return "Asphodelos: The Fourth Circle (Savage) - Phase 2";
+
+        }
+    }
 
     useEffect(() => {
     }, []);
 
-    return <div>Log</div>;
+    return (
+        <div className="grid h-screen w-full grid-cols-1 bg-slate-200 md:grid-cols-1 bg-gradient-to-r from-zinc-400 to-slate-400 justify-items-center gap-2">
+            <div className="bg-slate-200 w-8/12 h-fit text-center mt-4 rounded-lg py-2 drop-shadow-xl">
+                <h2 className="font-sans prose-h2 prose-2xl font-bold">{fightInstance()}</h2>
+            </div>
+            <div className="bg-slate-200 w-10/12 h-fit text-center mt-4 rounded-lg py-2 drop-shadow-xl">
+                <ul>
+                    {abilities.map((ability, index) => {
+                        if (ability.type === "cast") {
+                            return (
+                                <li key={index}>
+                                    <p className="font-sans prose-h3 prose-2xl font-bold">{ability.ability.name}</p>
+                                </li>
+                            )
+                        }
+                    })}
+                </ul>
+            </div>
+            {/* <ul>
+                {abilities.map((ability) => {
+                    return (
+                        <li>
+                            {ability.ability.name}
+                        </li>
+                    );
+                })}
+            </ul> */}
+        </div>
+    );
 }
